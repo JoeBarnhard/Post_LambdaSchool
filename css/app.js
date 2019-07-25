8b55c0b0a1749de60f1b943744730cdb147980cc
JoeBarnhard <52589890+JoeBarnhard@users.noreply.github.com>
2019-07-25 19:16:52 -0400

    more

---
+++ css/app.js
@@ -0,0 +1,20 @@
+// alert("hello"); 
+// console.log("hello world");  
+
+// var a = "hello world!";
+
+// let message = "welcome";
+// console.log(message);
+// if(message){
+//     message = "update message";
+// }
+// console.log(message);
+
+let message;
+console.log(message);
+message = true;
+if(true){
+    let message = "update message";
+    console.log(mesage);
+}
+console.log(message);
